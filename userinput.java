e26ef466b6ec2da6ef42f4f448472c94c7cfae2f
Eve <eveliina.korjus@edu.turkuamk.fi>
2018-04-09 23:11:48 +0300

    Muutettu localDate Dateksi, validoidaan käyttäjän päivämäärä ilman vuosilukua

--- userinput.java
+++ userinput.java
@@ -2,27 +2,21 @@ package horoskooppigeneraattori;
 
 import java.util.Scanner;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
 
 
 public class userinput {
 	private static final Scanner reader = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		System.out.println("Anna p‰iv‰m‰‰r‰: ");
-		String todayDate = reader.next();
-		System.out.println("todayDate: " + todayDate);
-		DateTimeFormatter sdf = DateTimeFormatter.ofPattern("d.M.yyyy");
-		
-		LocalDate date2=null;
-		try {
-		    //Parsing the String
-		    date2 = LocalDate.parse(todayDate, sdf);
-		} catch (Exception e) {
-		    // TODO Auto-generated catch block
-		    e.printStackTrace();
-		}
-		System.out.println("date2: " + date2);
+//		System.out.println("Anna p‰iv‰m‰‰r‰: ");
+//		String givenDate = reader.next();
+//		System.out.println("Given date: " + givenDate);
+
+	    Date date = askDate();
 		
         /*
         LocalDate date1 = LocalDate.of(2009, 12, 31);
@@ -34,6 +28,32 @@ public class userinput {
 		System.out.print(todayDate.isBefore(date2)); //prints true
 		System.out.print(date2.isBefore(date2)); //prints false
 */
+	}
+	
+	public static Date askDate() {
+	    SimpleDateFormat formatter = new SimpleDateFormat("d.M.");
+		
+	    Date date2=null;
+	    
+
+	    boolean validInput = false;
+	    while(!validInput) {
+			try {
+				System.out.println("Anna p‰iv‰m‰‰r‰: ");
+				String givenDate = reader.next();
+			    //Parsing the String
+			    date2 = formatter.parse(givenDate);
+			    validInput = true;
+	
+			} catch (Exception e) {
+			    // TODO Auto-generated catch block
+//			    e.printStackTrace();
+			    System.out.println("Ep‰kelpo p‰iv‰m‰‰r‰");
+
+			}
+	    }
+	    
+	    return date2;
 	}
 
 }
