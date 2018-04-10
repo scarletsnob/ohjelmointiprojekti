b95bb7f8b30b04a65b94de2794b6bd3977365ba7
Eve <eveliina.korjus@edu.turkuamk.fi>
2018-04-10 10:08:40 +0300

    Tarkistetaan onko annettu päivämäärä tammikuussa

--- userinput.java
+++ userinput.java
@@ -1,59 +1,61 @@
 package horoskooppigeneraattori;
 
 import java.util.Scanner;
-import java.time.LocalDate;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
-
 
 
 public class userinput {
 	private static final Scanner reader = new Scanner(System.in);
 
-	public static void main(String[] args) {
-//		System.out.println("Anna p‰iv‰m‰‰r‰: ");
-//		String givenDate = reader.next();
-//		System.out.println("Given date: " + givenDate);
+	public static void main(String[] args) throws ParseException {
 
-	    Date date = askDate();
+		Date givenDate = askDate();
 		
-        /*
-        LocalDate date1 = LocalDate.of(2009, 12, 31);
-        LocalDate date2 = LocalDate.of(2010, 01, 31);
-
-        System.out.println("todayDate : " + sdf.format(date1));
-        System.out.println("date2 : " + sdf.format(date2));
+		String startDateString = ("1.1.1970");
+	    String endDateString = ("1.2.1970");
+	    
+	    Date startDate = stringToDateConverter(startDateString);
+	    Date endDate  = stringToDateConverter(endDateString);
+	    
+	    System.out.println("Start Date: " + startDate);
+	    System.out.println("End Date: " + endDate);
+	    
+	    System.out.println(isWithinRange(givenDate, startDate, endDate));
 		
-		System.out.print(todayDate.isBefore(date2)); //prints true
-		System.out.print(date2.isBefore(date2)); //prints false
-*/
-	}
+        
+	}//main
 	
-	public static Date askDate() {
+	public static Date askDate() throws ParseException {
 	    SimpleDateFormat formatter = new SimpleDateFormat("d.M.");
 		
 	    Date date2=null;
-	    
-
 	    boolean validInput = false;
 	    while(!validInput) {
 			try {
-				System.out.println("Anna p‰iv‰m‰‰r‰: ");
+				System.out.println("Anna p‰iv‰m‰‰r‰ (muodossa pp.kk): ");
 				String givenDate = reader.next();
 			    //Parsing the String
 			    date2 = formatter.parse(givenDate);
 			    validInput = true;
 	
 			} catch (Exception e) {
-			    // TODO Auto-generated catch block
-//			    e.printStackTrace();
-			    System.out.println("Ep‰kelpo p‰iv‰m‰‰r‰");
-
-			}
-	    }
+			    System.out.println("Ep‰kelpo p‰iv‰m‰‰r‰.");
+			}//catch
+	    }//while
 	    
 	    return date2;
+	}// askDate
+	
+	static boolean isWithinRange(Date testDate, Date startDate, Date endDate) {
+//	    SimpleDateFormat formatter = new SimpleDateFormat("d.M.");
+	    return !(testDate.before(startDate) || testDate.after(endDate));
+		}//isWithinRange
+	
+	public static Date stringToDateConverter(String string) throws ParseException {
+		SimpleDateFormat format = new SimpleDateFormat("d.M.");
+		Date date = format.parse(string);
+		return date;
 	}
-
 }
