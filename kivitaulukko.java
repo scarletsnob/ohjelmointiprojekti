b55a8b188ac183ffe0f4d7cd5e1cb5649312e1bc
Hannelehkonen <33018290+Hannelehkonen@users.noreply.github.com>
2018-04-10 11:08:51 +0300

    Luotu aihio kivitaulukolle
    Tarkoitus tehdä tästä random generaattori onnenkivelle perustuen horoskooppimerkkiin.

---
+++ kivitaulukko.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class kivitaulukko {
+	
+	private static final Scanner lukija = new Scanner (System.in);
+
+	public static void main(String[] args) {
+		
+		
+		//Tehtava 1
+		
+				String[] taulu = { "Vesimies – Haukansilmä/Turkoosi", "Kalat – Ametisti/Krysopaasi", "Oinas – Jaspis/Karneoli", "Härkä – Karneoli/Ruusukvartsi", "Kaksonen – Sitriini/Tiikerinsilmä", "Rapu – Karneoli/Vihreä aventuriini", "Leijona – Spektroliitti/Vuorikide", "Neitsyt – Akaatti/Sitriini", "Vaaka – Kalsedoni/Savukvartsi/opaali/berylli/koralli/ruusukvartsi/aventuriini/timantti/", "Skorpioni – Karneoli/Sitriini", "Jousimies – Kalsedoni/Turkoosi", "Kauris – Jaspis/Onyks" };
+//				System.out.println(taulu);
+				System.out.println(taulu[1]);
+				
+//				boolean syote = lueLukuLajitellusta("Anna etsittava luku: ");
+//				System.out.print("Syottamasi luku " +syote +etsiLukua(syote, taulu) +"lajitellusta taulukosta. \n");
+//				*/
+	
+		//string[] taulu;
+
+		// yksittaisen alkion tulostus
+		//System.out.println("Sinun onnenkivesi on:");
+		//System.out.println(luvut[2]);
+		
+	}
+
+}
+
