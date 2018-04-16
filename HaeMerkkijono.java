fdcfa5946996478ab0666b46881e3fbd889e3351
Emma <emma.suomalainen@gmail.com>
2018-04-16 21:35:58 +0300

    Lisätty merkkijonon muuttaminen txt tiedostoksi ja sen haku

---
+++ HaeMerkkijono.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class HaeMerkkijono {
+
+	private static final Scanner lukija = new Scanner(System.in);
+	private static String outString;
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		System.out.println("Anna horokooppi: ");
+		String horoskooppi = lukija.next();
+		
+		Scanner in = new Scanner(new FileReader(horoskooppi + ".txt"));
+		StringBuilder sb = new StringBuilder();
+		while(in.hasNext()) {
+		    sb.append(in.next());
+		}
+		in.close();
+		outString = sb.toString();
+		System.out.println(outString);
+	}
+
+}
